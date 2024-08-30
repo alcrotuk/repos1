e51eb7de0a1aaf390eaf8a28c37bfdc953db36b7
Alexandr Crotuk <alcrotuk@yandex.ru>
2024-08-30 16:33:04 +0300

    file func

---
+++ task2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	char ch;
+	if (argc != 4) {
+		cout << "Usage: <file1> <file2> <file3>";
+		return 1;
+	}
+	ifstream f1(argv[1], ios::in | ios::binary);
+	if (!f1.is_open()) {
+		cout << "cannot open file1";
+		return 1;
+	}
+	ifstream f2(argv[2], ios::in | ios::binary);
+	if (!f2.is_open()) {
+		cout << "cannot open file2";
+		return 1;
+	}
+	ofstream fout(argv[3], ios::out | ios::binary);
+	if (!fout.is_open()) {
+		cout << "cannot open file3";
+		return 1;
+	}
+	while (f1.get(ch))
+	{
+		fout.put(ch);
+	}
+	while (f2.get(ch)) {
+		fout.put(ch);
+	}
+	f1.close();
+	f2.close();
+	fout.close();
+	return 0;
+}
